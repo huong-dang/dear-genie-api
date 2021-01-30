e073bbdc964bf6192c9da954e49a9d9b709a5f89
Huong Dang <huongd97@knights.ucf.edu>
2021-01-30 01:00:53 -0500

    Add errors constant

---
+++ utilities/errors.js
@@ -0,0 +1,11 @@
+const ERRORS = {
+    PASSWORD_LENGTH: "Password must be at least 8 characters",
+    PASSWORD_REQUIRED: "Password is required",
+    NAME_REQUIRED: "Name is required",
+    EMAIL_REQUIRED: "Email is required",
+    DATE_UPDATED_REQUIRED: "Date updated is required",
+    DATE_CREATED_REQUIRED: "Date created is required",
+    EMAIL_EXISTS: "{VALUE} already exists in our system",
+};
+
+module.exports = ERRORS;
